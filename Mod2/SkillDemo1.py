f6f72fbea1cdcc933fb34b15573db31f6b872ce6
Ryan Fagan <rfagan8964@gmail.com>
2025-04-21 00:21:16 -0700

    Added SkillsDemo

---
+++ Mod2/SkillDemo1.py
@@ -0,0 +1,7 @@
+#WE need 1. Name, 2. Total Bill, 3> People, 4. Total/People 5. 
+#Hey (name), each person in your (people)-person party will pay (result)
+name = input("what is your name? ")
+total = float(input("what is the total bill? "))
+partySize = int(input("What is the total number of people in your party? "))
+splitBill = (total / partySize)
+print("hello", name, "each person in your", partySize,f" pRyaerson party will need to pay ${splitBill:.2f}")
