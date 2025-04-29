248e5e16af5dd4580d10e9a8c8ec45f2da79a3af
Ryan Fagan <rfagan8964@gmail.com>
2025-04-29 10:52:03 -0700

    Doing my best.

---
+++ Mod3/problem3.py
@@ -0,0 +1,3 @@
+data = input("What data would you like me to search? ")
+info = input("What information would you like me to find? ")
+print (info in data)
---
+++ Mod3/problem4.py
@@ -0,0 +1,4 @@
+data = input("What data would you like me to search? ")
+idx1 = int(input("Where can I find the start of the information you want? "))
+idx2 = int(input("Where can I find the end of the information you want? "))
+print (data[idx1:idx2])
